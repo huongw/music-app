b9b992f280dcb08d37098630e489ee7fd4b39101
Whitney <designedbywh@gmail.com>
2023-03-06 16:50:09 -0500

    implement lyrics section for artists

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -5,14 +5,23 @@ export const shazamCoreApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "https://shazam-core.p.rapidapi.com",
     prepareHeaders: (headers) => {
-      headers.set("X-RapidAPI-Key", import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY);
+      headers.set(
+        "X-RapidAPI-Key",
+        import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY
+      );
 
       return headers;
     },
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => "/v1/charts/world" }),
+    getSongDetails: builder.query({
+      query: (songid) => `/v1/tracks/details?track_id=${songid}`,
+    }),
+    getSongRelated: builder.query({
+      query: (songid) => `/v1/tracks/related?track_id=${songid}`,
+    }),
   }),
 });
 
-export const { useGetTopChartsQuery } = shazamCoreApi;
+export const { useGetTopChartsQuery, useGetSongDetailsQuery, useGetSongRelatedQuery } = shazamCoreApi;
